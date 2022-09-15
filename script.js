c6c8c2c9e3dcb2c067691f7bef270ee2df10361d
“kara <bouncingpie@icloud.com>
2022-09-14 23:43:12 -0400

    added a function for the generator prompts

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  console.log("generatePassword")
+  var passwordNumbers = prompt("How many characters would you like your password to contain", 8);
+  console.log(passwordNumbers);
+  var specialCharacters = confirm("Click OK to confirm special characters");
+  var numericCharacters = confirm("Click OK to confirm including numeric numbers");
+  var lowerCase = confirm("Click OK to confirm using lowercase characters");
+  var upperCase = confirm("Click OK to confirm using uppercase characters");
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
