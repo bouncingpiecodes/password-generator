2a3d59d2edaf5b3d2d2742ce6d774335212ee2aa
“kara <bouncingpie@icloud.com>
2022-09-18 21:32:27 -0400

    added a for loop

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var characters = [".", "!", "@", "#", "$", "%", "^", "&", "*", ")", "(", "+", "=", "[", "]", "}", "{", ";", ":", "'", ">", "<", "?", "/", "~"];
+var letters = "abcdefghijklmnopqrstuvwxyz";
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 function generatePassword() {
   console.log("generatePassword")
   var passwordNumbers = prompt("How many characters would you like your password to contain", 8);
@@ -17,8 +18,11 @@ function generatePassword() {
   var numericCharacters = confirm("Click OK to confirm including numeric numbers");
   var lowerCase = confirm("Click OK to confirm using lowercase characters");
   var upperCase = confirm("Click OK to confirm using uppercase characters");
-
+for (x = 0; x < passwordNumbers; x ++) {
+  console.log(x);
 }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
