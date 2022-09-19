2040ba8a155cfee90e84308018b5c342b6227b6c
“kara <bouncingpie@icloud.com>
2022-09-18 22:00:49 -0400

    added math random to my loop and created option var

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var characters = [".", "!", "@", "#", "$", "%", "^", "&", "*", ")", "(", "+", "=", "[", "]", "}", "{", ";", ":", "'", ">", "<", "?", "/", "~"];
+var characters = [
+  ".",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  ")",
+  "(",
+  "+",
+  "=",
+  "[",
+  "]",
+  "}",
+  "{",
+  ";",
+  ":",
+  "'",
+  ">",
+  "<",
+  "?",
+  "/",
+  "~",
+];
 var letters = "abcdefghijklmnopqrstuvwxyz";
 // Write password to the #password input
 function writePassword() {
@@ -11,18 +37,40 @@ function writePassword() {
 }
 
 function generatePassword() {
-  console.log("generatePassword")
-  var passwordNumbers = prompt("How many characters would you like your password to contain", 8);
+  console.log("generatePassword");
+  var options = [];
+  var passwordNumbers = prompt(
+    "How many characters would you like your password to contain",
+    8
+  );
   console.log(passwordNumbers);
   var specialCharacters = confirm("Click OK to confirm special characters");
-  var numericCharacters = confirm("Click OK to confirm including numeric numbers");
+  if (specialCharacters) {
+    options.push("specialCharacters");
+  }
+  var numericCharacters = confirm(
+    "Click OK to confirm including numeric numbers"
+  );
+  if (numericCharacters) {
+    options.push("numericCharacters");
+  }
   var lowerCase = confirm("Click OK to confirm using lowercase characters");
+  if (lowerCase) {
+    options.push("lowerCase");
+  }
   var upperCase = confirm("Click OK to confirm using uppercase characters");
-for (x = 0; x < passwordNumbers; x ++) {
-  console.log(x);
-}
-}
+  if (upperCase) {
+    options.push("upperCase");
+  }
+  var password = "";
+  console.log(options);
 
+  for (x = 0; x < passwordNumbers; x++) {
+    console.log(x);
+    var option = options[Math.floor(Math.random() * options.length)];
+    console.log(option);
+  }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
